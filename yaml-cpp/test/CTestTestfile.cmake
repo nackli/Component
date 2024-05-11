7212bda13cc15f4f0d3469de09af49ca0fb1f7c0
nack li <nackli@163.com>
2024-05-11 16:50:57 +0800

    删除yaml-cpp文件夹

--- yaml-cpp/test/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/ComponentDev/yaml-cpp/test
-# Build directory: E:/ComponentDev/yaml-cpp/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(yaml-cpp::test "yaml-cpp-tests")
-set_tests_properties(yaml-cpp::test PROPERTIES  _BACKTRACE_TRIPLES "E:/ComponentDev/yaml-cpp/test/CMakeLists.txt;49;add_test;E:/ComponentDev/yaml-cpp/test/CMakeLists.txt;0;")
-subdirs("prefix")
